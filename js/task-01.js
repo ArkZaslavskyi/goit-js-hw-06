911893cfa0814341ea245ab3d442220dff446a26
ArkZaslavskyi <ark.zaslavskyi@gmail.com>
2022-05-25 14:07:26 +0300

    task 02

--- js/task-01.js
+++ js/task-01.js
@@ -4,7 +4,7 @@ const categoriesRef = document.querySelector('#categories');
 // numbers of categories children ()
 console.log(`Number of categories: ${categoriesRef.children.length}`);
 
-// 
+// iterating over the children of ul#categories
 [...categoriesRef.children].forEach(child => {
     // reference on category title
     const currentCategoryTitleRef = child.firstElementChild;
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// function for make one ingredient item  
+const makeIngredientItem = (ingredient) => {
+  const ingredientItem = document.createElement('li');
+  ingredientItem.classList.add('item');
+  ingredientItem.textContent = ingredient;
+  return ingredientItem;
+}
+
+// make list of items
+const ingredientsList = ingredients.map(makeIngredientItem);
+
+// get reference on #ingredients
+const ingredientsRef = document.querySelector('#ingredients');
+
+// append all items to #ingredients
+ingredientsRef.append(...ingredientsList);
