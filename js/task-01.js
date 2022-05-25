77654924a87dbd4bcd61c1161c4c36279e92a4e9
ArkZaslavskyi <ark.zaslavskyi@gmail.com>
2022-05-25 10:32:15 +0300

    Initial commit

---
+++ js/task-01.js
@@ -0,0 +1,16 @@
+// reference on #categories (top level ul)
+const categoriesRef = document.querySelector('#categories');
+
+// numbers of children ()
+console.log(`Number of categories: ${categoriesRef.children.length}`);
+
+// 
+[...categoriesRef.children].forEach(child => {
+    // reference on category title
+    const currentCategoryTitleRef = child.firstElementChild;
+    console.log(`\nCategory: ${currentCategoryTitleRef.textContent}`);
+
+    // reference on category list
+    const currentCategoryListRef = child.lastElementChild;
+    console.log(`Elements: ${currentCategoryListRef.children.length}`);
+});
