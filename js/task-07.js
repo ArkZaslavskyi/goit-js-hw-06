d7a0e15483337c39ca569b3409ea587b9e01eda3
ArkZaslavskyi <ark.zaslavskyi@gmail.com>
2022-05-26 23:36:30 +0300

    task-7

---
+++ js/task-07.js
@@ -0,0 +1,18 @@
+/*
+ * initialising References
+ */
+
+// reference on range field "#font-size-control"
+const rangeInputRef = document.querySelector('#font-size-control');
+
+// reference on text "#text"
+const textRef = document.querySelector('#text');
+
+// initialising values (36)
+rangeInputRef.value = 36;
+textRef.style.fontSize = rangeInputRef.value+"px";
+
+// listener on input range field
+rangeInputRef.addEventListener('input', (event) => {
+    textRef.style.fontSize = event.currentTarget.value+"px";
+});
