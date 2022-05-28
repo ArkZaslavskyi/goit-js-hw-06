d9a2b9a57224ff3f55ce6546772789f859bef0ab
ArkZaslavskyi <ark.zaslavskyi@gmail.com>
2022-05-28 22:06:47 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,29 @@
+/*
+ * Напиши скрипт, который изменяет цвета фона элемента < body > через инлайн стиль при клике на button.change - color и выводит значение цвета в span.color.
+ * Для генерации случайного цвета используй функцию getRandomHexColor.
+ */
+
+/*
+ * initialising References
+ */
+const btnChangeColorRef = document.querySelector('.widget .change-color');
+const bodyRef = document.querySelector('body');
+const textColorRef = document.querySelector('.widget .color');
+
+// on button click listener
+btnChangeColorRef.addEventListener('click', onBtnChangeColor);
+
+// on button click handle
+function onBtnChangeColor() {
+  textColorRef.textContent =
+    getRandomHexColor()
+    .toString()
+    .toUpperCase();
+  bodyRef.style.backgroundColor = textColorRef.textContent;
+};
+
+// 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)}`;
 }
