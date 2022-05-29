0bdb438f7dc029eecf1ab5d0ff14b76b96058019
ArkZaslavskyi <ark.zaslavskyi@gmail.com>
2022-05-29 13:37:15 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,20 +7,22 @@ const ingredients = [
   'Condiments',
 ];
 
-// function to make single ingredient item
-// pattern: <li class="item">ingredient<li> 
-const makeIngredientItem = (ingredient) => {
-  const ingredientItem = document.createElement('li');
-  ingredientItem.classList.add('item');
-  ingredientItem.textContent = ingredient;
-  return ingredientItem;
-}
+// get reference on #ingredients
+const ingredientsRef = document.querySelector('#ingredients');
 
 // make list of items
 const ingredientsList = ingredients.map(makeIngredientItem);
 
-// get reference on #ingredients
-const ingredientsRef = document.querySelector('#ingredients');
-
 // append all items to #ingredients
 ingredientsRef.append(...ingredientsList);
+
+// function to make single ingredient item
+// pattern: <li class="item">ingredient<li> 
+function makeIngredientItem (ingredient) {
+  const ingredientItem = document.createElement('li');
+
+  ingredientItem.classList.add('item');
+  ingredientItem.textContent = ingredient;
+
+  return ingredientItem;
+}
